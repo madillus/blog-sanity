ab1d1a879e882838769a1e80d2e05630555c126b
madillus <68782132+madillus@users.noreply.github.com>
2022-10-31 19:43:05 +0100

    Deleted unused files

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -30,6 +30,7 @@ export default async function createComment(
       comment,
     })
   } catch (err) {
+    console.error(err)
     return res.status(500).json({ message: `Couldn't submit comment`, err })
   }
 

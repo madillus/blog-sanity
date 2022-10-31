ea02daf34f3832f19b05901ac81417df229dec28
madillus <68782132+madillus@users.noreply.github.com>
2022-10-31 13:24:08 +0100

    Fixed a typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </>
